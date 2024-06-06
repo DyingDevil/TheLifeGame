972b044e0efc056a8ccdaf21ff6ee56f35a27461
DyingDevil <avilovve@yandex.ru>
2024-06-06 19:35:59 +0300

    settings.c

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -2,11 +2,43 @@
 #include<stdlib.h>
 #include <stdio.h>
 #include "settings.h"
-int load_settings(const char *filename, int set[3]){
-	printf("load_settings\n");
-	return 0;
+int set_settings(const char *filename, int mas[MATRIX2_SIZE]) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Ошибка открытия файла: %s\n", filename);
+        return -20;
+    }
+
+    for (int i = 0; i < MATRIX2_SIZE; ++i) {
+       
+            if (fscanf(file, "%d", &mas[i]) != 1) {
+                printf("Ошибка чтения из файла\n");
+                fclose(file);
+                return -20;
+            }
+    }
+
+    fclose(file);
+    return 0;
 }
-int save_settings(const char *filename, int set[3]){
-	printf("save_settings\n");
-	return 0;
+
+int load_game(const char *filename, int matrix[MATRIX1_SIZE][MATRIX1_SIZE]) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Ошибка открытия файла: %s\n", filename);
+        return -20;
+    }
+
+    for (int i = 0; i < MATRIX1_SIZE; ++i) {
+        for (int j = 0; j < MATRIX1_SIZE; ++j) {
+            if (fscanf(file, "%d", &matrix[i][j]) != 1) {
+                printf("Ошибка чтения из файла\n");
+                fclose(file);
+                return -20;
+            }
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
